a69d4f0fe64f674aa4997b6549548c5872b4255a
chrisaor <pchpch0070@gmail.com>
2018-02-21 16:26:44 +0900

    post_add view, url, template 생성 및 base.html에서 연결 링크 구현

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.post_list, name='post-list'),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('add/', views.post_add, name='post-add'),
 ]
